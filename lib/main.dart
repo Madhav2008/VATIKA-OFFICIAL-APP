d3a655d8eb9a8a4cb68f71e3acfbd9994c5883e2
Madhav2008 <aroramadhav65@gmail.com>
2023-01-29 19:06:17 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
-      home: const LoginPage(),
+      home: LoginPage(),
     );
   }
 }
